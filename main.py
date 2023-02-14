1580498c9b83003e2bda2d4d0b92ef22cfc9e2a2
kiarash <shahedikiarash@gmail.com>
2023-02-14 14:30:34 +0330

    return date and time fast api

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
+@app.get("/")
+def get_datetime():
+    return {"datetime": datetime.now()}
 
-
-@app.get('/')
-def index():
-    return 'hu'
